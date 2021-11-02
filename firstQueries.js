f1d947bbe0d8a529504cfda809f759eb4bbbadfa
Roberto Garcia <41040915+naranjito72@users.noreply.github.com>
2021-11-02 12:09:57 +0100

    Update firstQueries.js

--- firstQueries.js
+++ firstQueries.js
@@ -3,8 +3,10 @@ db.getCollection('restaurants_list').find().pretty().limit(20);
 db.getCollection('restaurants_list').find({"cuisine": "Spanish"}).pretty();
 db.getCollection('restaurants_list').find({"restaurant_id": "30112340"}).pretty();
 db.getCollection('restaurants_list').find({"name": "May May Kitchen"}).pretty();
-db.getCollection('restaurants_list').find({"cuisine": "Tapas"}).pretty();
-// ordered by tapas and name sort
+db.getCollection('restaurants_list').find({"cuisine": "Tapas"}).pretty();
+
+// ordered by tapas and name sort
+
 db.getCollection('restaurants_list').find({ "cuisine": "Tapas"}).sort({ "name": 1}).pretty();
 db.getCollection('restaurants_list').find({"address.zipcode": "11208"}).pretty();
 db.getCollection('restaurants_list').find({"grades.score": {$gte: 70}}).pretty();
@@ -21,7 +23,7 @@ db.getCollection('restaurants_list').find({$or: [
                                                  {"cuisine": "Chilean"},
                                                  {"cuisine": "Czech"}   
                                                     ]}).pretty();
-db.getCollection('restaurants_list').find({"grades.grade": "B"}).pretty();// to be review it
+db.getCollection('restaurants_list').find({"grades.grade.1": "B"}).pretty();// to be review it
 db.getCollection('restaurants_list').aggregate([ { $project : { grades : 1} } ])
 db.getCollection('restaurants_list').find({$and: [
                                                  {"grades.grade": "Z"},
