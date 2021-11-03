1f02c07af995af33a8bcf6b33a0fcd28894c02b6
Roberto Garcia <41040915+naranjito72@users.noreply.github.com>
2021-11-03 08:06:48 +0100

    Update firstQueries.js

--- firstQueries.js
+++ firstQueries.js
@@ -1,16 +1,22 @@
 db.getCollection('restaurants_list').find().pretty().limit(20);
 
 db.getCollection('restaurants_list').find({"cuisine": "Spanish"}).pretty();
+
 db.getCollection('restaurants_list').find({"restaurant_id": "30112340"}).pretty();
+
 db.getCollection('restaurants_list').find({"name": "May May Kitchen"}).pretty();
+
 db.getCollection('restaurants_list').find({"cuisine": "Tapas"}).pretty();
 
 // ordered by tapas and name sort
-
 db.getCollection('restaurants_list').find({ "cuisine": "Tapas"}).sort({ "name": 1}).pretty();
+
 db.getCollection('restaurants_list').find({"address.zipcode": "11208"}).pretty();
+
 db.getCollection('restaurants_list').find({"grades.score": {$gte: 70}}).pretty();
+
 db.getCollection('restaurants_list').find({"borough": "Brooklyn"}).pretty();
+
 db.getCollection('restaurants_list').find({$and: [
                                                  {"borough": "Brooklyn"},
                                                  {"grades.score": {$gt: 70}}   
@@ -23,6 +29,7 @@ db.getCollection('restaurants_list').find({$or: [
                                                  {"cuisine": "Chilean"},
                                                  {"cuisine": "Czech"}   
                                                     ]}).pretty();
+
 db.getCollection('restaurants_list').find({"grades.1.grade": "B"}).pretty();
 // to access positions we must use dots and no []
 
@@ -32,4 +39,8 @@ db.getCollection('restaurants_list').find({$and: [
                                                  {"grades.grade": "Z"},
                                                  {"grades.grade": "B"}   
                                                     ]}).pretty();
-db.restaurats.find({})
+
+db.restaurants.find( { "grades.date" : ISODate("2014-09-16") }).pretty();
+
+db.getCollection('restaurants_list').find( { "grades.date" : { $gt: ISODate("2015-01-01") } } ).count();
+
